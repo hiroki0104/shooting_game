6b506e8d4d51e19fbbc644422ddcd7dd84ab67fd
hiroki0104 <hiroki010419@outlook.jp>
2020-04-04 03:24:51 +0900

    create_canvas_util&&first_render

---
+++ script/script.js
@@ -0,0 +1,33 @@
+;(() => {
+  const CANVAS_WIDTH = 640
+  const CANVAS_HEIGHT = 480
+
+  let util, canvas, ctx, image
+
+  window.addEventListener('load', () => {
+    util = new Canvas2DUtility(document.getElementById('main_canvas'))
+    canvas = util.canvas
+    ctx = util.context
+
+    util.imageLoader('./../img/viper.png', loadedImage => {
+      image = loadedImage
+      initialize()
+      render()
+    })
+  })
+
+  function initialize() {
+    canvas.width = CANVAS_WIDTH
+    canvas.height = CANVAS_HEIGHT
+  }
+
+  function render() {
+    util.drawRact(0, 0, canvas.width, canvas.height, '#eeeeee')
+    ctx.drawImage(image, 100, 100)
+  }
+
+  function generateRandomInt(range) {
+    let random = Math.random()
+    return Math.floor(random * range)
+  }
+})()
